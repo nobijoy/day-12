1b3dbfe9c536f7e0020a3ec8e5820eebe564a825
nobijoy <nobijoy1446@gmail.com>
2022-02-01 09:22:01 +0600

    calculator

---
+++ app/classes/Calculator.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\classes;
+class Calculator
+{
+    public $firstNumber;
+    public $secondNumber;
+    public $operator;
+    public $result;
+
+    public function index(){
+        echo 'Hello world';
+    }
+
+
+
+}
